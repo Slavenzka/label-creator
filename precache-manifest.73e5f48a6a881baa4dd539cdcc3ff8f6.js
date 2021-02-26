b803da718361f6c0aef35fd66e165ba763b9f92b
Ivan <i.romanov@protonmail.com>
2021-02-26 18:21:32 +0300

    Updates

---
+++ precache-manifest.73e5f48a6a881baa4dd539cdcc3ff8f6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46968ea531bcf4b891f30315c277133d",
+    "url": "/label-creator/index.html"
+  },
+  {
+    "revision": "bc343ff9b7e551489b15",
+    "url": "/label-creator/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ba93824dc9b9247f4875",
+    "url": "/label-creator/static/css/main.18e1ceee.chunk.css"
+  },
+  {
+    "revision": "bc343ff9b7e551489b15",
+    "url": "/label-creator/static/js/2.eb9c65af.chunk.js"
+  },
+  {
+    "revision": "92276d17dfba12ead0c52a029fcff031",
+    "url": "/label-creator/static/js/2.eb9c65af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba93824dc9b9247f4875",
+    "url": "/label-creator/static/js/main.f4529764.chunk.js"
+  },
+  {
+    "revision": "f2276182726fcbdfe4d0",
+    "url": "/label-creator/static/js/runtime-main.8a52cc96.js"
+  },
+  {
+    "revision": "231495e51444c162bed5df57ee968c8e",
+    "url": "/label-creator/static/media/FuturaPT-Book.231495e5.woff2"
+  },
+  {
+    "revision": "54a123e5410e5dadf16d8b368fa0ab54",
+    "url": "/label-creator/static/media/FuturaPT-Book.54a123e5.woff"
+  },
+  {
+    "revision": "081741b541afdaa972cb5f76ac6d3a5e",
+    "url": "/label-creator/static/media/FuturaPT-ExtraBold.081741b5.woff2"
+  },
+  {
+    "revision": "11ba3ac6bb29be79323bda60fb72e9fb",
+    "url": "/label-creator/static/media/FuturaPT-ExtraBold.11ba3ac6.woff"
+  },
+  {
+    "revision": "7813a81a9a31f54d193bbfdc4cec0e63",
+    "url": "/label-creator/static/media/FuturaPT-Light.7813a81a.woff2"
+  },
+  {
+    "revision": "c89fa27abbcba4de1d9606fdfed9dcce",
+    "url": "/label-creator/static/media/FuturaPT-Light.c89fa27a.woff"
+  },
+  {
+    "revision": "8fa18513e8b92c123707ed12dc54b9da",
+    "url": "/label-creator/static/media/FuturaPT-Medium.8fa18513.woff"
+  },
+  {
+    "revision": "9819eaf042d4ad45186a5b49dfae95b4",
+    "url": "/label-creator/static/media/FuturaPT-Medium.9819eaf0.woff2"
+  },
+  {
+    "revision": "04b82b15da4bf7682d0944a396365143",
+    "url": "/label-creator/static/media/FuturaPTBold.04b82b15.woff2"
+  },
+  {
+    "revision": "b1755e863dafc58f2dd34f8ba8f7479b",
+    "url": "/label-creator/static/media/FuturaPTBold.b1755e86.woff"
+  },
+  {
+    "revision": "1726f096cc123b7eaa1351f200227471",
+    "url": "/label-creator/static/media/FuturaPTDemi.1726f096.woff"
+  },
+  {
+    "revision": "29fa49c1fce0e2d03503cfee4a50e9f9",
+    "url": "/label-creator/static/media/FuturaPTDemi.29fa49c1.woff2"
+  }
+]);
